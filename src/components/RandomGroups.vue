d2a574039c9fa16bf1d8834d136d1d22c9570bf6
ZAlex1007 <zambaalexandru1@gmail.com>
2021-01-07 19:48:45 +0200

    Removed default value from Random Groups

--- src/components/RandomGroups.vue
+++ src/components/RandomGroups.vue
@@ -10,7 +10,7 @@
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item" v-for="n in teamsPerSeed" :key="n">
                         <input  type="text" class="form-control" @change="resetCSS(i.toString()+n.toString())" 
-                            placeholder="Insert team name" :id="i.toString()+n.toString()" autocomplete="off" :value="'a'+i.toString()+n.toString()">
+                            placeholder="Insert team name" :id="i.toString()+n.toString()" autocomplete="off">
                     </li>
                 </ul>
             </div>
